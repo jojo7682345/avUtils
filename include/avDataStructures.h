d13cda0e2dce6ce27c4367a7540a99fd9a61f63c
jojo7682345 <44713644+jojo7682345@users.noreply.github.com>
2023-10-30 22:01:54 +0100

    adding template for grid datastructure

--- include/avDataStructures.h
+++ include/avDataStructures.h
@@ -2,5 +2,8 @@
 #define __AV_DATA_STRUCTURES__
 
 #include "avQueue.h"
+#include "avGrid.h"
+//#include "avList.h"
+//#include "avMap.h"
 
 #endif //__AV_DATA_STRUCTURES__
---
+++ include/avGrid.h
@@ -0,0 +1,8 @@
+#ifndef __AV_GRID__
+#define __AV_GRID__
+
+#include "avTypes.h"
+
+typedef struct AvGrid_T* AvGrid;
+
+#endif//__AV_GRID__
