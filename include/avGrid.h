a9f941a4c096d8e333eb4e1d63046f59809ecce7
jojo7682345 <44713644+jojo7682345@users.noreply.github.com>
2023-10-30 22:22:45 +0100

    added basic grid functions

--- include/avGrid.h
+++ include/avGrid.h
@@ -5,4 +5,44 @@
 
 typedef struct AvGrid_T* AvGrid;
 
+/// <summary>
+/// creates a grid instance
+/// </summary>
+/// <param name="elementSize">: the size of the elements in the grid</param>
+/// <param name="width">: the  width of the grid</param>
+/// <param name="height">: the height of the grid</param>
+/// <param name="grid">: the grid handle</param>
+void avGridCreate(uint64 elementSize, uint32 width, uint32 height, AvGrid* grid);
+
+/// <summary>
+/// write to the grid
+/// </summary>
+/// <param name="data"> pointer of the data you want to be written</param>
+/// <param name="x"></param>
+/// <param name="y"></param>
+/// <param name="grid"> the grid handle</param>
+void avGridWrite(void* data, uint32 x, uint32 y, AvGrid grid);
+
+/// <summary>
+/// read from the grid
+/// </summary>
+/// <param name="data"> pointer to where the data needs to be stored </param>
+/// <param name="x"></param>
+/// <param name="y"></param>
+/// <param name="grid"> the grid handle</param>
+void avGridRead(void* data, uint32 x, uint32 y, AvGrid grid);
+
+/// <summary>
+/// clear the grid
+/// </summary>
+/// <param name="data"> clear value</param>
+/// <param name="grid"> the grid handle</param>
+void avGridClear(void* data, AvGrid grid);
+
+/// <summary>
+/// destroys the grid instance
+/// </summary>
+/// <param name="grid"></param>
+void aGridDestroy(AvGrid grid);
+
 #endif//__AV_GRID__
--- src/avGrid.c
+++ src/avGrid.c
@@ -1,1 +1,84 @@
 #include <avGrid.h>
+#include <avMemory.h>
+#include <string.h>
+
+typedef struct AvGrid_T {
+	byte* data;
+	const uint64 elementSize;
+	uint32 width;
+	uint32 height;
+} AvGrid_T;
+
+void avGridCreate(uint64 elementSize, uint32 width, uint32 height, AvGrid* grid) {
+	if (elementSize == 0) {
+		return;
+	}
+	if (width == 0 || height == 0) {
+		return;
+	}
+
+	(*grid) = avAllocate(sizeof(AvGrid_T), "allocating grid handle");
+
+	memcpy((void*)&((*grid)->elementSize), &elementSize, sizeof(elementSize));
+	(*grid)->width = width;
+	(*grid)->height = height;
+
+	uint64 elementCount = (uint64)width * (uint64)height;
+
+	(*grid)->data = avCallocate(elementCount, elementSize, "allocating grid memory");
+}
+
+static bool32 checkBounds(uint32 x, uint32 y, AvGrid grid) {
+	return x < grid->width && y < grid->height;
+}
+
+static uint64 getIndex(uint32 x, uint32 y, AvGrid grid) {
+	return (uint64)y * (uint64)grid->width + (uint64)x;
+}
+
+static void* getPtr(uint32 x, uint32 y, AvGrid grid) {
+	return grid->data + grid->elementSize * getIndex(x,y,grid);
+}
+
+static uint64 getElementCount(AvGrid grid) {
+	return (uint64)grid->width * (uint64)grid->height;
+}
+
+void avGridWrite(void* data, uint32 x, uint32 y, AvGrid grid) {
+	if (!checkBounds(x, y, grid)) {
+		return;
+	}
+	if (data == NULL) {
+		return;
+	}
+
+	memcpy(getPtr(x, y, grid), data, grid->elementSize);
+}
+
+void avGridRead(void* data, uint32 x, uint32 y, AvGrid grid) {
+	if (!checkBounds(x, y, grid)) {
+		return;
+	}
+	if (data == NULL) {
+		return;
+	}
+	memcpy(data, getPtr(x, y, grid), grid->elementSize);
+}
+
+void avGridClear(void* data, AvGrid grid) {
+	if (data == NULL) {
+		memset(grid->data, 0, getElementCount(grid) * grid->elementSize);
+		return;
+	}
+	
+	for(uint32 y = 0; y < grid->height; y++){
+		for (uint32 x = 0; x < grid->width; x++) {
+			avGridWrite(data, x, y, grid);
+		}
+	}
+}
+
+void aGridDestroy(AvGrid grid) {
+	avFree(grid->data);
+	avFree(grid);
+}
