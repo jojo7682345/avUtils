494177d308433fba705bc950f88d7e37355cd330
jojo7682345 <44713644+jojo7682345@users.noreply.github.com>
2024-01-13 23:20:58 +0100

    type difference between linux and windows

--- include/AvUtils/avTypes.h
+++ include/AvUtils/avTypes.h
@@ -9,39 +9,60 @@ C_SYMBOLS_START
 #define nullptr ((void*)0UL)
 #endif
 
+#define avUint unsigned int
+#define avUint32 unsigned int
+
+#ifdef _WIN32
+#define avUint64 unsigned long long
+#else
+#define avUint64 unsigned long
+#endif
+
+#define avUint16 unsigned short
+#define avUint8 unsigned char
+#define avByte unsigned char
+
+#define avInt32 int
+#define avInt64 long
+#define avInt16 short
+#define avInt8 char
+
+#define avBool8 unsigned char
+#define avBool32 unsigned int
+
 #ifndef AV_DISABLE_TYPES
 
-typedef unsigned int uint;
-typedef unsigned int uint32;
-typedef unsigned long long uint64;
-typedef unsigned short uint16;
-typedef unsigned char uint8;
-typedef unsigned char byte;
+typedef avUint uint;
+typedef avUint32 uint32;
+typedef avUint64 uint64;
+typedef avUint16 uint16;
+typedef avUint8 uint8;
+typedef avByte byte;
 
-typedef int int32;
-typedef long int64;
-typedef short int16;
-typedef char int8;
+typedef avInt32 int32;
+typedef avInt64 int64;
+typedef avInt16 int16;
+typedef avInt8 int8;
 
-typedef byte bool8;
-typedef uint32 bool32;
+typedef avBool8 bool8;
+typedef avBool32 bool32;
 
 #else
 
-#define uint unsigned int
-#define uint32 unsigned int
-#define uint64 unsigned long
-#define uint15 unsigned short
-#define uint8 unsigned char
-#define byte unsigned char
+#define uint avUint
+#define uint32 avUint32
+#define uint64 avUint64
+#define uint16 avUint16
+#define uint8 avUint8
+#define byte avByte
 
-#define int32 int
-#define int64 long
-#define int16 short
-#define int8 char
+#define int32 avInt32
+#define int64 avInt64
+#define int16 avInt16
+#define int8 avInt8
 
-#define bool8 unsigned char
-#define bool32 unsigned int
+#define bool8 avBool8
+#define bool32 avBool32
 
 #endif
 
